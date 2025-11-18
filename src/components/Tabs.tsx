1b5d76e8ecaf60763ebc962882fda9440e9cc15e
Johan Burell <johan.burell@gmail.com>
2025-11-18 23:55:18 +0100

    Turn off the news feed

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -6,24 +6,24 @@ import AdventOfCode from "./AdventOfCode"
 
 const enum ActiveTab {
   Videos,
-  Feed,
-  AoC
+  // Feed,
+  // AoC
 }
 
 export default function Tabs() {
-  const [activeTab, setTab] = useState<ActiveTab>(ActiveTab.Feed);
+  const [activeTab, setTab] = useState<ActiveTab>(ActiveTab.Videos);
 
   return (
     <div className={styles.tab_container}>
       <div className={styles.content_radio_container}>
-        <div className={activeTab == ActiveTab.Feed ? styles.content_radio_active : styles.content_radio} onClick={() => setTab(ActiveTab.Feed)}>Feed</div>
+        {/* <div className={activeTab == ActiveTab.Feed ? styles.content_radio_active : styles.content_radio} onClick={() => setTab(ActiveTab.Feed)}>Feed</div> */}
         <div className={activeTab == ActiveTab.Videos ? styles.content_radio_active : styles.content_radio} onClick={() => setTab(ActiveTab.Videos)}>Videos</div>
-        <div className={activeTab == ActiveTab.AoC ? styles.content_radio_active : styles.content_radio} onClick={() => setTab(ActiveTab.AoC)}>Advent Of Code 2025</div>
+        {/* <div className={activeTab == ActiveTab.AoC ? styles.content_radio_active : styles.content_radio} onClick={() => setTab(ActiveTab.AoC)}>Advent Of Code 2025</div> */}
       </div>
       <div className={styles.content_main}>
-        {(activeTab == ActiveTab.Feed) && <Feed />}
+        {/* {(activeTab == ActiveTab.Feed) && <Feed />} */}
         {(activeTab == ActiveTab.Videos) && <Videos />}
-        {(activeTab == ActiveTab.AoC) && <AdventOfCode />}
+        {/* {(activeTab == ActiveTab.AoC) && <AdventOfCode />} */}
       </div>
     </div>
   );
