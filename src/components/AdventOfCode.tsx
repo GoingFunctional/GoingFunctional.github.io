c9278ce7c0ebc87a952b404cc70b2293a013f9d1
Johan Burell <johan.burell@gmail.com>
2025-11-21 02:51:23 +0100

    WIP: Working with AoC/GH integrations

--- src/components/AdventOfCode.tsx
+++ src/components/AdventOfCode.tsx
@@ -3,8 +3,10 @@ import styles from "./AdventOfCode.module.css";
 
 type Profile = {
   id: number;
-  name: string;
+  name: string | null;
+  login: string;
   location: string | null;
+  bio: string | null;
   profile_url: string;
   profile_pic_url: string | null;
   solutions: Solution[] | null;
@@ -64,13 +66,21 @@ export default function Profiles() {
       const fetched = await Promise.all(
         users.map(async (u) => {
           try {
-            const r = await fetch(PROFILE_BASE_URL + u.userName);
+            const r = await fetch((PROFILE_BASE_URL + u.userName),
+              {
+                method: "GET",
+                headers: {
+                  "Authorization": "Bearer <add>"
+                }
+              });
             if (!r.ok) return null;
             const d = await r.json();
             return {
               id: d.id,
-              name: d.name ?? d.login,
+              name: d.name,
+              login: d.login,
               location: d.location,
+              bio: d.bio,
               profile_url: d.html_url,
               profile_pic_url: d.avatar_url,
               solutions: u.solutions,
@@ -84,21 +94,31 @@ export default function Profiles() {
   }, [users]);
 
   return (
-    <div className={styles.container} style={{ paddingBottom: "1rem", height: "100%", width: "100%" }}>
+    <div className={styles.container}>
       {profiles.map((it) => (
         <div className={styles.content_container} key={it.id} >
           <div className={styles.content_header}>
-            <h3>{it.name}</h3>
-            <h2>{it.location}</h2>
-            <img className={styles.avatar} alt={it.name} src={it.profile_pic_url ?? "adabeat_logo.png"}></img>
+            <div className={styles.header_container}>
+              <div className={styles.header_avatar_container}>
+                <a href={it.profile_url} >
+                  <img className={styles.avatar} alt={it.login} src={it.profile_pic_url ?? "user-question.svg"}></img>
+                </a>
+              </div>
+              <div className={styles.header_info}>
+                <a href={it.profile_url}><h2>{it.name != null && it.name + " "}{it.name != null && "("}<span className={styles.login}>{it.login}</span>{it.name && ")"}</h2></a>
+                <h3>{it.location}</h3>
+                <div className={styles.bio}>{it.bio}</div>
+              </div>
+            </div>
           </div>
-          <div className={styles.content_main}>
+          <div className={styles.content_wrap} >
+            {(it.solutions == null || it.solutions.length == 0) && <div className={styles.solution_row} key="0"><div className={styles.no_solutions}>[No solutions uploaded yet]</div></div>}
             {it.solutions?.sort((a, b) => (a.day - b.day)).map((s) => (
-              <div key={s.day}>
-                <div>Day: {s.day}</div>
-                {s.writeUp && <div><a href={s.writeUp} >Read more</a></div>}
-                {s.solution1 && <div><a href={s.solution1} >Solution 1</a></div>}
-                {s.solution2 && <div><a href={s.solution2} >Solution 2</a></div>}
+              <div className={styles.solution_row} key={s.day}>
+                <a href={"https://adventofcode.com/2024/day/" + s.day} ><div className={styles.solution_day_header}><h2>Day: {s.day}</h2></div></a>
+                {s.writeUp && <div className={styles.read_more}><a href={s.writeUp} >Write-up</a></div>}
+                {s.solution1 && <div className={styles.solution_content}><a href={s.solution1} >Solution 1</a></div>}
+                {s.solution2 && <div className={styles.solution_content}><a href={s.solution2} >Solution 2</a></div>}
               </div>
             ))}
           </div>
