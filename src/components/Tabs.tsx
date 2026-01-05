cc3b43db30187fe3c52d07e48cb787f513005844
Johan Burell <johan.burell@gmail.com>
2026-01-05 01:09:18 +0100

    Added news feeds back

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -6,7 +6,7 @@ import AdventOfCode from "./AdventOfCode"
 
 const enum ActiveTab {
   Videos,
-  // Feed,
+  Feed,
   AoC
 }
 
@@ -16,12 +16,12 @@ export default function Tabs() {
   return (
     <div className={styles.tab_container}>
       <div className={styles.content_radio_container}>
-        {/* <div className={activeTab == ActiveTab.Feed ? styles.content_radio_active : styles.content_radio} onClick={() => setTab(ActiveTab.Feed)}>Feed</div> */}
+        <div className={activeTab == ActiveTab.Feed ? styles.content_radio_active : styles.content_radio} onClick={() => setTab(ActiveTab.Feed)}>Feed</div>
         <div className={activeTab == ActiveTab.Videos ? styles.content_radio_active : styles.content_radio} onClick={() => setTab(ActiveTab.Videos)}>Videos</div>
         <div className={activeTab == ActiveTab.AoC ? styles.content_radio_active : styles.content_radio} onClick={() => setTab(ActiveTab.AoC)}>Advent Of Code 2025</div>
       </div>
       <div className={styles.content_main}>
-        {/* {(activeTab == ActiveTab.Feed) && <Feed />} */}
+        {(activeTab == ActiveTab.Feed) && <Feed />}
         {(activeTab == ActiveTab.Videos) && <Videos />}
         {(activeTab == ActiveTab.AoC) && <AdventOfCode />}
       </div>
